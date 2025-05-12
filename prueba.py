9e26c9395539824a356b065a554299843651ce31
Mauricio Andrés Zamora Hernández <64853106+mauricio-zamora-ucr@users.noreply.github.com>
2025-05-12 21:01:59 +0000

    acutalizacion

--- prueba.py
+++ prueba.py
@@ -1,1 +1,7 @@
-print('hola mundo desde codespace')
+import pandas as pd
+import numpy as np
+
+# Ejemplo: Producción diaria (en unidades)
+produccion = pd.Series([100, 120, 110, 130, 115, 125, 120],
+                       index=['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'])
+print(produccion)
